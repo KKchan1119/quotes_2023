faed76f52c99e701fa5f0e3c1c84897b028d687f
KKchan1119 <kkchan1119@gmail.com>
2023-03-02 14:06:31 +0900

    5번 분리완

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,62 +1,29 @@
 package org.example;
 
-import org.example.wiseList.entity.WiseList;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import org.example.system.controller.SystemController;
+import org.example.wiseList.controller.WiseListController;
 public class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
 
-    private final Scanner sc;
+        SystemController systemController = new SystemController();
+        WiseListController wiseListController = new WiseListController();
 
-    public App(Scanner sc){
-        this.sc = sc;
-    }
+        while (true) {
+            System.out.printf("명령) ");
+            //trim() = 좌우 공백 제거
+            String cmd = Container.getScanner().nextLine().trim();
 
-        public void run() {
-            int count = 0;
-
-            System.out.println("== 명언 앱 ==");
-            List<WiseList> wiseList = new ArrayList<>();
-
-            while(true) {
-                System.out.printf("명령) ");
-                //trim() = 좌우 공백 제거
-                String cmd = sc.nextLine().trim();
-                if(cmd.equals("등록")){
-                    int id = count +1;
-                    System.out.printf("명언 : ");
-                    String input = sc.nextLine();
-                    System.out.printf("작가 : ");
-                    String writer = sc.nextLine();
-                    WiseList wiseList1 = new WiseList(count, input, writer);
-                    wiseList.add(wiseList1);
-                    count = id;
-                    System.out.println(count+"번 명언이 등록되었습니다.");
-                }
-
-                if(cmd.equals("목록")){
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("-".repeat(30));
-                    for(int i = 0; i < wiseList.size(); i++) {
-                        WiseList wiseList1 = wiseList.get(i);
-
-                        System.out.printf("%d / %s / %s\n", wiseList1.getId(), wiseList1.getWriterName(), wiseList1.getContent());
-
-                    }
-                }
-                if(cmd.equals("삭제")){
-
-                }
-
-
-
-                if(cmd.equals("종료")){
-                    break;
-                }
+            if (cmd.equals("등록")) {
+                wiseListController.write();
+            } else if (cmd.equals("목록")) {
+                wiseListController.list();
+            } else if (cmd.equals("종료")) {
+                systemController.exit();
+                break;
             }
+        }
 
     }
-
 }
+
--- src/main/java/org/example/wiseList/controller/WiseListController.java
+++ src/main/java/org/example/wiseList/controller/WiseListController.java
@@ -1,4 +1,40 @@
 package org.example.wiseList.controller;
 
+import org.example.wiseList.entity.WiseList;
+import org.example.Container;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WiseListController {
+    private int count;
+    private final List<WiseList> wiseList;
+
+    public WiseListController(){
+        count = 0;
+        wiseList = new ArrayList<>();
+    }
+    public void write(){
+        int id = count +1;
+        System.out.printf("명언 : ");
+        String input = Container.getScanner().nextLine();
+        System.out.printf("작가 : ");
+        String writer = Container.getScanner().nextLine();
+        WiseList wiseList1 = new WiseList(count, input, writer);
+        wiseList.add(wiseList1);
+        count = id;
+        System.out.println(count+"번 명언이 등록되었습니다.");
+    }
+
+    public void list(){
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-".repeat(30));
+        for(int i = 0; i < wiseList.size(); i++) {
+            WiseList wiseList1 = wiseList.get(i);
+
+            System.out.printf("%d / %s / %s\n", wiseList1.getId(), wiseList1.getWriterName(), wiseList1.getContent());
+
+        }
+    }
 }
+
